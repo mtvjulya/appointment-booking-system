beeb538fa63a526c56180a9864ac21ce5d4cccbd
mtvjulya <mtvjulya@yahoo.com>
2026-04-14 23:32:42 +0100

    Fix tests: use H2 in-memory database for testing

--- backend/appointments/src/test/java/ie/gov/appointments/AppointmentsApplicationTests.java
+++ backend/appointments/src/test/java/ie/gov/appointments/AppointmentsApplicationTests.java
@@ -2,8 +2,10 @@ package ie.gov.appointments;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AppointmentsApplicationTests {
 
 	@Test
