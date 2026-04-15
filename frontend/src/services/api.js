6f3e7b7134fabbe110eecd0c42d31b094f58dfa6
mtvjulya <mtvjulya@yahoo.com>
2026-04-15 11:30:49 +0100

    Update API URL to HTTPS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://16.170.252.193:8080/api',
+  baseURL: 'https://16.170.252.193/api',
   headers: {
     'Content-Type': 'application/json',
   },
